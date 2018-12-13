2766a287f0cda348ed1ccfc364ec0f919615f124
Mathieu Velten <--unset>
2018-12-13 22:20:16 +0100

    Implements GetMediaInfo using ffprobe

--- MediaBrowser.Controller/MediaEncoding/MediaEncoder.cs
+++ MediaBrowser.Controller/MediaEncoding/MediaEncoder.cs
@@ -19,9 +19,47 @@ using MediaBrowser.Model.MediaInfo;
 using MediaBrowser.Model.Reflection;
 using MediaBrowser.Model.Serialization;
 using MediaBrowser.Model.System;
+using System.Diagnostics;
+using System.Text;
+using Newtonsoft.Json;
 
 namespace MediaBrowser.Controller.MediaEncoding
 {
+    public class FFProbeResult
+    {
+        public class Stream
+        {
+            public class Disposition {
+                [JsonProperty("default")]
+                public bool _default { get; set; }
+                public bool forced { get; set; }
+            }
+            public Disposition disposition { get; set; }
+            public int index { get; set; }
+            public string codec_type { get; set; }
+            public string codec_name { get; set; }
+            public string codec_long_name { get; set; }
+            public string profile { get; set; }
+            public int width { get; set; }
+            public int height { get; set; }
+            public int channels { get; set; }
+            public string channel_layout { get; set; }
+            public Dictionary<string, string> tags { get; set; }
+        }
+        public class Format
+        {
+            public string filename { get; set; }
+            public string format_long_name { get; set; }
+            public string format_name { get; set; }
+            public double duration { get; set; }
+            public long size { get; set; }
+            public int bit_rate { get; set; }
+
+        }
+        public IList<Stream> streams { get; set; }
+        public Format format { get; set; }
+    }
+
     public class MediaEncoder : IMediaEncoder
     {
         private ILogger logger;
@@ -135,12 +173,65 @@ namespace MediaBrowser.Controller.MediaEncoding
 
         string IMediaEncoder.GetInputArgument(string[] inputFiles, MediaProtocol protocol)
         {
-            throw new NotImplementedException();
+            logger.Error("GetInputArgument");
+            if (inputFiles.Length <= 0) {
+                return null;
+            }
+            if (protocol == MediaProtocol.File) {
+                return "file:" + inputFiles[0];
+            }
+            return null;
         }
 
         Task<MediaInfo> IMediaEncoder.GetMediaInfo(MediaInfoRequest request, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            return Task.Run(function: () => {
+                Process p = new Process();
+                p.StartInfo.RedirectStandardOutput = true;
+                p.StartInfo.RedirectStandardError = true;
+                p.StartInfo.FileName   = "ffprobe";
+                p.StartInfo.Arguments = "-i file:" + Quote(request.MediaSource.Path);
+                p.StartInfo.Arguments += " -threads 0 -v info -print_format json -show_streams -show_chapters -show_format";
+                p.Start();
+                p.WaitForExit(1000);
+
+                FFProbeResult res = JsonConvert.DeserializeObject<FFProbeResult>(p.StandardOutput.ReadToEnd());
+
+                MediaInfo info = new MediaInfo();
+
+                info.Bitrate = res.format.bit_rate;
+                info.Size = res.format.size;
+                info.Container = res.format.format_name;
+
+                foreach (FFProbeResult.Stream s in res.streams) {
+                    MediaStream stream = new MediaStream();
+
+                    if (s.disposition != null) {
+                        stream.IsDefault = s.disposition._default;
+                        stream.IsForced = s.disposition.forced;
+                    }
+
+                    if ("video".Equals(s.codec_type)) {
+                        stream.Type = MediaStreamType.Video;
+                        stream.Height = s.height;
+                        stream.Width = s.width;
+                    } else if ("audio".Equals(s.codec_type)) {
+                        stream.Type = MediaStreamType.Audio;
+                        stream.Channels = s.channels;
+                        stream.ChannelLayout = s.channel_layout;
+                    } else if ("subtitle".Equals(s.codec_type)) {
+                        stream.Type = MediaStreamType.Subtitle;
+                    }
+
+                    stream.Index = s.index;
+                    stream.Codec = s.codec_name;
+                    stream.Profile = s.profile;
+                    if (s.tags.ContainsKey("language"))
+                        stream.Language = s.tags["language"];
+                    info.MediaStreams.Add(stream);
+                }
+                return info;
+            }, cancellationToken: cancellationToken);
         }
 
         string[] IMediaEncoder.GetPlayableStreamFileNames(string path, VideoType videoType)
@@ -185,6 +276,54 @@ namespace MediaBrowser.Controller.MediaEncoding
         public Task<string> GetSubtitleFileCharacterSet(string path, string language, MediaProtocol protocol, CancellationToken cancellationToken)
         {
             throw new NotImplementedException();
+        }
+
+        public static string Quote(string arg) {
+            StringBuilder sb = new StringBuilder();
+            //foreach (string arg in args) {
+                int backslashes = 0;
+
+                // Add a space to separate this argument from the others
+                if (sb.Length > 0) {
+                    sb.Append(" ");
+                }
+
+                bool needquote = arg.Length == 0 || arg.Contains(" ") || arg.Contains("\t");
+                if (needquote) {
+                    sb.Append('"');
+                }
+
+                foreach (char c in arg) {
+                    if (c == '\\') {
+                        // Don't know if we need to double yet.
+                        backslashes++;
+                    }
+                    else if (c == '"') {
+                        // Double backslashes.
+                        sb.Append(new String('\\', backslashes*2));
+                        backslashes = 0;
+                        sb.Append("\\\"");
+                    } else {
+                        // Normal char
+                        if (backslashes > 0) {
+                            sb.Append(new String('\\', backslashes));
+                            backslashes = 0;
+                        }
+                        sb.Append(c);
+                    }
+                }
+
+                // Add remaining backslashes, if any.
+                if (backslashes > 0) {
+                    sb.Append(new String('\\', backslashes));
+                }
+
+                if (needquote) {
+                    sb.Append(new String('\\', backslashes));
+                    sb.Append('"');
+                }
+            //}
+            return sb.ToString();
         }
     }
 }
